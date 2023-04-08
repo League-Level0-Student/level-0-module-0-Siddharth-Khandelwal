44fb6a8aa54ab969ca39db12d088870f996d8982
league <league@imac-14.attlocal.net>
2023-04-08 16:22:50 -0700

    calculator

--- src/CalculatorAddOrSubtract.java
+++ src/CalculatorAddOrSubtract.java
@@ -1,15 +1,19 @@
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CalculatorAddOrSubtract implements ActionListener {
 	JButton add = new JButton(" Addition(+) ");
 	JButton subtract = new JButton("Subtraction(-)");
+	JButton multiply = new JButton("Multiply(x)");
+	JButton division = new JButton("Division(/)");
 	JTextField num1 = new JTextField(10);
 	JTextField num2 = new JTextField(10);
 	JLabel label1 = new JLabel("Number 1:");
@@ -20,6 +24,7 @@ t.run();
 }	
 	void run(){
 		JFrame frame = new JFrame();
+		frame.setPreferredSize(new Dimension(500,100));
 	JPanel panel = new JPanel();
 	frame.add(panel);
 	panel.add(label1);
@@ -28,23 +33,37 @@ t.run();
 	panel.add(num2);
 	panel.add(add);
 	panel.add(subtract);
+	panel.add(multiply);
+	panel.add(division);
 	
 	frame.setVisible(true);
 	frame.pack();
 	add.addActionListener(this);
 	subtract.addActionListener(this);
+	multiply.addActionListener(this);
+	division.addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 		String number1 = num1.getText();
-		int one;
-		one = Integer.parseInt(number1);
+		double one = Double.parseDouble(number1);
 		String number2 = num2.getText();
-		int two;
-		two = Integer.parseInt(number2);
+		double two = Double.parseDouble(number2);
 		JButton pressed = (JButton) e.getSource();
-	}
+		if (pressed == add){
+			JOptionPane.showMessageDialog(null, one+two);
+		}
+		else if (pressed == subtract) {
+			JOptionPane.showMessageDialog(null, one-two);
+		}
+		else if (pressed == multiply) {
+			JOptionPane.showMessageDialog(null, one*two);
+		}
+		else if(pressed == division) {
+			JOptionPane.showMessageDialog(null, one/two);
+		}
 
+	}
 }
