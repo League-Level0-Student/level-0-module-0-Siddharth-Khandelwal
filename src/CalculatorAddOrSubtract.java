bcde88aacc1baa49661dcae8fc1f42d07c01b8d0
root <root@c7e02b1c078a>
2023-04-14 01:28:38 +0000

    calculator, added sqaring and cubing

--- src/CalculatorAddOrSubtract.java
+++ src/CalculatorAddOrSubtract.java
@@ -10,14 +10,17 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class CalculatorAddOrSubtract implements ActionListener {
-	JButton add = new JButton(" Addition(+) ");
+	JButton add = new JButton("Addition(+)");
 	JButton subtract = new JButton("Subtraction(-)");
-	JButton multiply = new JButton("Multiply(x)");
+	JButton multiply = new JButton("Multiplication(x)");
 	JButton division = new JButton("Division(/)");
+	JButton square = new JButton("Square(^2)");
+	JButton cube = new JButton("Cube(^3)");
 	JTextField num1 = new JTextField(10);
 	JTextField num2 = new JTextField(10);
 	JLabel label1 = new JLabel("Number 1:");
 	JLabel label2 = new JLabel("Number 2:");
+	JLabel info = new JLabel("If squaring or cubing, Number 2 must be filled, but is irrelevant.");
 public static void main(String[] args) {
 CalculatorAddOrSubtract t = new CalculatorAddOrSubtract();
 t.run();
@@ -35,6 +38,9 @@ t.run();
 	panel.add(subtract);
 	panel.add(multiply);
 	panel.add(division);
+	panel.add(info);
+	panel.add(square);
+	panel.add(cube);
 	
 	frame.setVisible(true);
 	frame.pack();
@@ -42,6 +48,8 @@ t.run();
 	subtract.addActionListener(this);
 	multiply.addActionListener(this);
 	division.addActionListener(this);
+	square.addActionListener(this);
+	cube.addActionListener(this);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -53,17 +61,22 @@ t.run();
 		double two = Double.parseDouble(number2);
 		JButton pressed = (JButton) e.getSource();
 		if (pressed == add){
-			JOptionPane.showMessageDialog(null, one+two);
+			JOptionPane.showMessageDialog(null, "Addition Answer:"+(one+two));
 		}
 		else if (pressed == subtract) {
-			JOptionPane.showMessageDialog(null, one-two);
+			JOptionPane.showMessageDialog(null, "Subtraction Answer:"+(one-two));
 		}
 		else if (pressed == multiply) {
-			JOptionPane.showMessageDialog(null, one*two);
+			JOptionPane.showMessageDialog(null, "Multiplication Answer:"+(one*two));
 		}
 		else if(pressed == division) {
-			JOptionPane.showMessageDialog(null, one/two);
+			JOptionPane.showMessageDialog(null, "Division Answer:"+(one/two));
 		}
-
+		else if(pressed == square) {
+			JOptionPane.showMessageDialog(null, "Squaring Answer:"+(one*one));
+		}
+		else if(pressed == cube) {
+			JOptionPane.showMessageDialog(null, "Cubing Answer:"+(one*one*one));
+		}
 	}
 }
