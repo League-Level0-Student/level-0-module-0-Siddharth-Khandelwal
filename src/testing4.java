fc18abff92ad2a50d0db9f17cf0a4b182673ba5e
root <root@b738d087a021>
2023-04-28 01:22:21 +0000

    testing 4

--- src/testing4.java
+++ src/testing4.java
@@ -3,11 +3,15 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class testing4 implements ActionListener{
-JButton button = new JButton();
+JButton button = new JButton("Click after inserting number.");
+JTextField textfield = new JTextField(10);
+JLabel label = new JLabel("Enter a number 1-30");
 	public static void main(String[] args) {
 	testing4 t = new testing4();
 	t.run();
@@ -16,15 +20,24 @@ JButton button = new JButton();
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	frame.add(panel);
+	panel.add(label);
+	panel.add(textfield);
 	panel.add(button);
 	frame.pack();
+	frame.setVisible(true);
 	button.addActionListener(this);
 	
 	}
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		JButton pressed = (JButton) e.getSource();
+		String first = textfield.getText();
+		double one = Double.parseDouble(first);
+		double two = 999999999;
+	if(pressed == button) {
+		}
 	}
-}
+		}
+
 
