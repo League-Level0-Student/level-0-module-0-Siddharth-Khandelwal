75667ba6cad10849acf1a6474f8bdab4c1298dc5
league <league@imac-14.attlocal.net>
2023-04-29 16:29:53 -0700

    testing4 prettymuch done

--- src/testing4.java
+++ src/testing4.java
@@ -1,5 +1,6 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -11,7 +12,7 @@ import javax.swing.JTextField;
 public class testing4 implements ActionListener{
 JButton button = new JButton("Click after inserting number.");
 JTextField textfield = new JTextField(10);
-JLabel label = new JLabel("Enter a number 1-30");
+JLabel label = new JLabel("Enter a number 1-300");
 	public static void main(String[] args) {
 	testing4 t = new testing4();
 	t.run();
@@ -33,11 +34,22 @@ JLabel label = new JLabel("Enter a number 1-30");
 		// TODO Auto-generated method stub
 		JButton pressed = (JButton) e.getSource();
 		String first = textfield.getText();
-		double one = Double.parseDouble(first);
-		double two = 999999999;
+		Random rand = new Random();
+				int one = Integer.parseInt(first);
 	if(pressed == button) {
-		}
+		int num = rand.nextInt(9)+2;
+		
+	if(one%num==0) {
+		JOptionPane.showMessageDialog(null, "you are special");
+		System.out.println("Your number divided by "+num+" had a remainder of 0");
 	}
-		}
+	else {
+		JOptionPane.showMessageDialog(null, "L, not special lol");
+		System.out.println("Your number divided by "+num+" did not have a remainder of 0");
+	}
+	}
+	}
+	}
+		
 
 
