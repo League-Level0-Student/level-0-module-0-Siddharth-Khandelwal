c18dd6a6d01fd15d46b83bc25e552cdbd14caf7a
league <league@imac-14.attlocal.net>
2023-05-20 16:28:59 -0700

    testing 5 completed

--- src/testing5.java
+++ src/testing5.java
@@ -2,9 +2,14 @@ import java.awt.Component;
 import java.awt.Robot;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.MalformedURLException;
+import java.net.URL;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
@@ -13,13 +18,26 @@ public class testing5 implements ActionListener {
 	JButton button1 = new JButton("Special Button :)");
 	JButton button2 = new JButton("Another button");
 	JButton button3 = new JButton("");
-	String imageAdd = "https://m.media-amazon.com/images/I/71o67mthJqL._UL1500_.jpg";
-	Component image = createImage(imageAdd);
+	JLabel image;
 public static void main(String[] args) {
 	testing5 t = new testing5();
 	t.run();
 	}
+String imageAdd = "https://m.media-amazon.com/images/I/61rf05mfTSS._UX569_.jpg";
+
+private JLabel createImage(String imageUrl) {
+	JLabel imageLabel = null;
+	try {
+	URL url = new URL(imageUrl);
+	Icon icon = new ImageIcon(url);
+	imageLabel = new JLabel(icon);
 	
+	}
+	catch(MalformedURLException e) {
+		
+	}
+	return imageLabel;
+}
 	void run(){
 	
 	JPanel panel = new JPanel();
@@ -30,7 +48,8 @@ public static void main(String[] args) {
 	frame.pack();
 	frame.setVisible(true);
 	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	
+	image = 	createImage(imageAdd);
+		
 	button1.addActionListener(this);
 	button2.addActionListener(this);
 	button3.addActionListener(this);
@@ -46,9 +65,9 @@ public static void main(String[] args) {
 		JOptionPane.showMessageDialog(null, "you wanted braids!");
 	}
 	if(pre == button3) {
-		JOptionPane.showMessageDialog(null, "");
+	frame.add(image);
 		
-	
+	frame.pack();
 	}
 	}
 }
