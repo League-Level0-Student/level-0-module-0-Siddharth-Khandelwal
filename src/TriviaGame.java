d1632004feadff60fa743544d9982ed525cb828a
root <root@11f98dcfadd8>
2023-06-27 02:52:34 +0000

    trivia game

--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -4,6 +4,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class TriviaGame implements ActionListener {
@@ -15,28 +16,52 @@ public class TriviaGame implements ActionListener {
 	JLabel label1 = new JLabel("Easy:");
 	JLabel label2 = new JLabel("Medium:");
 	JLabel label3 = new JLabel("Hard:");
-public static void main(String[] args) {
-	TriviaGame t = new TriviaGame();
-	t.run();
-}
-void run() {
-	frame.add(panel);
-	panel.add(label1);
-	panel.add(button1);
-	panel.add(label2);
-	panel.add(button2);
-	panel.add(label3);
-	panel.add(button3);
-	frame.pack();
-	frame.setVisible(true);
-	
-	button1.addActionListener(this);
-	button2.addActionListener(this);
-	button3.addActionListener(this);
-}
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+
+	public static void main(String[] args) {
+		TriviaGame t = new TriviaGame();
+		t.run();
+	}
+
+	void run() {
+		frame.add(panel);
+		panel.add(label1);
+		panel.add(button1);
+		panel.add(label2);
+		panel.add(button2);
+		panel.add(label3);
+		panel.add(button3);
+		frame.pack();
+		frame.setVisible(true);
+
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		JButton pressed = (JButton) e.getSource();
+		if (pressed == button1) {
+			String easy1 = JOptionPane.showInputDialog("Question 1: What is the capital of Russia?");
+			if (easy1.equalsIgnoreCase("moscow")) {
+				JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+				String easy2 = JOptionPane.showInputDialog("Question 2: What character is Tom Holland in the Marvel Chacter Universe?");
+				if (easy2.equalsIgnoreCase("Spiderman")) {
+					JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+					String easy3 = JOptionPane.showInputDialog("What sport does Lebron James play?");
+					if(easy3.equalsIgnoreCase("basketball")) {
+						JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+					}
+					else {
+						JOptionPane.showMessageDialog(null, "Incorrect");	
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Incorrect");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Incorrect");
+			}
+		}
+	}
 }
