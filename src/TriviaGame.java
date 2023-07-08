18fa47b632256e7d96251f4d6b095f6606a3bb9a
league <league@imac-14.attlocal.net>
2023-07-08 16:32:37 -0700

    trivia game med done

--- src/TriviaGame.java
+++ src/TriviaGame.java
@@ -43,10 +43,10 @@ public class TriviaGame implements ActionListener {
 		// TODO Auto-generated method stub
 		JButton pressed = (JButton) e.getSource();
 		if (pressed == button1) {
-			String easy1 = JOptionPane.showInputDialog("Question 1: What is the capital of Russia?");
-			if (easy1.equalsIgnoreCase("moscow")) {
+			String easy1 = JOptionPane.showInputDialog("Question 1: What is the capital of Italy?");
+			if (easy1.equalsIgnoreCase("rome")) {
 				JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
-				String easy2 = JOptionPane.showInputDialog("Question 2: What character is Tom Holland in the Marvel Chacter Universe?");
+				String easy2 = JOptionPane.showInputDialog("Question 2: What character is Tom Holland in the Marvel Cinematic Universe?(No hyphen)");
 				if (easy2.equalsIgnoreCase("Spiderman")) {
 					JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
 					String easy3 = JOptionPane.showInputDialog("What sport does Lebron James play?");
@@ -58,12 +58,10 @@ public class TriviaGame implements ActionListener {
 							String easy5 = JOptionPane.showInputDialog("Who was the first man on the Moon?");
 							if (easy5.equalsIgnoreCase("Neil Armstrong")) {
 								JOptionPane.showMessageDialog(null, "Correct! You have completed easy trivia game!");
-							}
-							else {
+							} else {
 								JOptionPane.showMessageDialog(null, "Incorrect SCORE:4");
 							}
-							}
-						else {
+						} else {
 							JOptionPane.showMessageDialog(null, "Incorrect SCORE:3");
 						}
 					} else {
@@ -76,5 +74,40 @@ public class TriviaGame implements ActionListener {
 				JOptionPane.showMessageDialog(null, "Incorrect SCORE:0");
 			}
 		}
+		if (pressed == button2) {
+			String med1 = JOptionPane.showInputDialog("What city was destroyed first by American atomic bombs in WWII?");
+			if (med1.equalsIgnoreCase("Hiroshima")) {
+				JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+				String med2 = JOptionPane.showInputDialog("What is the only mammal that can't jump?");
+				if (med2.equalsIgnoreCase("elephant")) {
+					JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+					String med3 = JOptionPane.showInputDialog("What is arachmophobia a fear of?(Plural)");
+					if (med3.equalsIgnoreCase("spiders")) {
+						JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+						String med4 = JOptionPane.showInputDialog("What modern day country is Chernobyl in?");
+						if (med4.equalsIgnoreCase("Ukraine")) {
+							JOptionPane.showMessageDialog(null, "Correct! Click below for next Question");
+							String med5 = JOptionPane
+									.showInputDialog("What country possesed Alaska before the United States?");
+							if (med5.equalsIgnoreCase("Russia")) {
+								JOptionPane.showMessageDialog(null, "Correct! You have completed medium trivia game!");
+							} else {
+								JOptionPane.showMessageDialog(null, "Incorrect SCORE:4");
+							}
+						} else {
+							JOptionPane.showMessageDialog(null, "Incorrect SCORE:3");
+						}
+					} else {
+						JOptionPane.showMessageDialog(null, "Incorrect SCORE:2");
+					}
+				} else {
+					JOptionPane.showMessageDialog(null, "Incorrect SCORE:1");
+				}
+			} else {
+				JOptionPane.showMessageDialog(null, "Incorrect SCORE:0");
+			}
+
+		}
+		if (pressed == button3)
 	}
 }
